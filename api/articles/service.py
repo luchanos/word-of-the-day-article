fb24dd1238b9fc7d905b55db59261bd7b4749e6e
luchanos <lukash_91@mail.ru>
2024-07-13 04:23:10 +0200

    init

--- api/articles/service.py
+++ api/articles/service.py
@@ -11,23 +11,30 @@ logger = logging.getLogger(__name__)
 
 class GetArticleHandler:
     GENERATE_ARTICLE_PROMPT_TEMPLATE = """
-Generate an article about the word '%s'."
+Generate an article about the word '{awad}'."
 In the response I want to see json-file with two keys:
 1. header - here you need to generate a header for article with max length 50 symbols.
-2. body - an article about the word '%s' with max length 300 symbols.
+2. body - an article about the word '{awad}' with max length 300 symbols.
 """
 
     async def __call__(self, app: FastAPIWithContext) -> GetArticleResponseModel:
-        awad = await app.redis_client.get_cached_word()
-        if awad is None:
+        article = await app.redis_client.get_cached_article()
+        if article is not None:
+            result = GetArticleResponseModel(
+                actual_date=date.today(),
+                header=article['header'],
+                body=article['body'],
+            )
+        else:
             awad = await app.wordsmith_client.get_awad()
-            await app.redis_client.cache_article(awad)
-        response = await app.openai_client.make_prompt_request(
-            prompt=self.GENERATE_ARTICLE_PROMPT_TEMPLATE % (awad, ))
-        content = response.json()['choices'][0]['message']['content']
-        response_data = json.loads(content)
-        return GetArticleResponseModel(
-            actual_date=date.today(),
-            header=response_data['header'],
-            body=response_data['body'],
-        )
+            response = await app.openai_client.make_prompt_request(
+                prompt=self.GENERATE_ARTICLE_PROMPT_TEMPLATE.format(awad=awad))
+            content = response.json()['choices'][0]['message']['content']
+            response_data = json.loads(content)
+            await app.redis_client.cache_article(header=response_data["header"], article=response_data["body"])
+            result = GetArticleResponseModel(
+                actual_date=date.today(),
+                header=response_data['header'],
+                body=response_data['body'],
+            )
+        return result
