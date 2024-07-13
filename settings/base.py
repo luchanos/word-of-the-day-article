33e766a525d08f1dd7e0e46daf28f321083164f3
luchanos <lukash_91@mail.ru>
2024-07-13 21:08:51 +0200

    init

--- settings/base.py
+++ settings/base.py
@@ -4,8 +4,3 @@ API_PORT = env.int("API_PORT", default=8080)
 
 OPENAI_API_KEY = env.str("OPENAI_API_KEY", default="")
 OPENAI_TEST_API_KEY = env.str("OPENAI_TEST_API_KEY", default="openai_test_api_key")
-
-REDIS_TEST_HOST = env.str("REDIS_TEST_HOST", default="0.0.0.0")
-REDIS_TEST_PORT = env.int("REDIS_TEST_PORT", default=6379)
-REDIS_HOST = env.str("REDIS_HOST", default="")
-REDIS_PORT = env.int("REDIS_PORT", default="")
